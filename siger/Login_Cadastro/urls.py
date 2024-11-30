c15e36b290eba950a046212a32736def339e7ad3
FabricioS2 <fabricio.g@escolar.ifrn.edu.br>
2024-11-30 17:42:39 -0300

    telas de login e cadastro

--- siger/Login_Cadastro/urls.py
+++ siger/Login_Cadastro/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('', views.Login, name="Login")
+   path('', views.Login, name="Login"),
+   path('Cadastro_aluno', views.Cadastro_aluno, name="Cadastro_aluno"),
+   path('Cadastro_admistrador', views.Cadastro_admistrador, name="Cadastro_admistrador"),
 ]
--- siger/Login_Cadastro/views.py
+++ siger/Login_Cadastro/views.py
@@ -7,4 +7,11 @@ from django.http import HttpResponse
 def Login(request):
    return render(request, 'Login_Cadastro/login.html')
 
+def Cadastro_aluno(request):
+   return render(request, 'Login_Cadastro/Cadastro_aluno.html')
+
+
+def Cadastro_admistrador(request):
+   return render(request, 'Login_Cadastro/Cadastro_admistrador.html')
+
 
